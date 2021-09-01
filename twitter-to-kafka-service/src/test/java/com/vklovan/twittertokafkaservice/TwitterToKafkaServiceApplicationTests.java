7a45284b1e81a2fb7cf9805a16a23a4d06ee6151
savak1990 <slava.klovan@gmail.com>
2021-09-01 07:21:00 +0300

    Initial commit with first microservice

---
+++ twitter-to-kafka-service/src/test/java/com/vklovan/twittertokafkaservice/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vklovan.twittertokafkaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
